2f7ac32bbc717ae1babf2195b40e5a29d1d1fe59
abdullahsaeed1211 <abdullah.saeed1724@gmail.com>
2026-05-11 22:19:14 +0530

    fix: skip MongoDB seeding when extension not loaded

--- database/seeders/ContentSeeder.php
+++ database/seeders/ContentSeeder.php
@@ -2,15 +2,17 @@
 
 namespace Database\Seeders;
 
-use App\Models\Mongo\BlogPost;
-use App\Models\Mongo\PageSEO;
 use Illuminate\Database\Seeder;
-use MongoDB\BSON\ObjectId;
 
 class ContentSeeder extends Seeder
 {
     public function run(): void
     {
+        if (! extension_loaded('mongodb')) {
+            $this->command->warn('MongoDB extension not loaded, skipping content seed.');
+            return;
+        }
+
         $this->seedBlogs();
         $this->seedPageSEO();
     }
@@ -20,7 +22,6 @@ class ContentSeeder extends Seeder
         $blogsPath = content_path('blogs.json');
         if (! file_exists($blogsPath)) {
             $this->command->warn('blogs.json not found, skipping blog seed.');
-
             return;
         }
 
@@ -29,11 +30,12 @@ class ContentSeeder extends Seeder
             return;
         }
 
+        $model = new \App\Models\Mongo\BlogPost;
         foreach ($blogs as $blog) {
-            BlogPost::updateOrCreate(
+            $model::updateOrCreate(
                 ['slug' => $blog['slug']],
                 [
-                    '_id' => $blog['id'] ?? (string) new ObjectId,
+                    '_id' => $blog['id'] ?? (string) new \MongoDB\BSON\ObjectId,
                     'title' => $blog['title'],
                     'slug' => $blog['slug'],
                     'author' => $blog['author'] ?? 'A1 Training',
@@ -47,7 +49,7 @@ class ContentSeeder extends Seeder
             );
         }
 
-        $this->command->info('Seeded '.count($blogs).' blog posts.');
+        $this->command->info('Seeded ' . count($blogs) . ' blog posts.');
     }
 
     protected function seedPageSEO(): void
@@ -55,7 +57,6 @@ class ContentSeeder extends Seeder
         $seoPath = content_path('pages_seo.json');
         if (! file_exists($seoPath)) {
             $this->command->warn('pages_seo.json not found, skipping SEO seed.');
-
             return;
         }
 
@@ -64,14 +65,15 @@ class ContentSeeder extends Seeder
             return;
         }
 
+        $model = new \App\Models\Mongo\PageSEO;
         foreach ($pages as $slug => $data) {
             $data['slug'] = $slug;
-            PageSEO::updateOrCreate(
+            $model::updateOrCreate(
                 ['slug' => $slug],
                 $data
             );
         }
 
-        $this->command->info('Seeded '.count($pages).' SEO pages.');
+        $this->command->info('Seeded ' . count($pages) . ' SEO pages.');
     }
 }
